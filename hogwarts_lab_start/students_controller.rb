6bb8791467716848914f51f4db1180b2122195df
Ernest Fraiman <upapaus@gmail.com>
2018-11-07 16:35:59 +0000

    amended addresses

--- hogwarts_lab_start/students_controller.rb
+++ hogwarts_lab_start/students_controller.rb
@@ -5,42 +5,42 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
-  get '/students' do
+  get '/students/?' do
     @students = Student.all()
     erb(:index)
   end
 
 # new
-  get '/students/new' do
+  get '/students/new/?' do
     erb(:new)
   end
 # show
-get '/students/:id' do
+get '/students/:id/?' do
   @student = Student.find(params[:id])
   erb(:show)
 end
 
 # create
-post '/students' do
+post '/students/?' do
   @student = Student.new(params)
   @student.save()
   redirect to '/students'
 end
 # edit
-  get '/students/:id/edit' do
+  get '/students/:id/edit/?' do
     @houses = House.all
     @student = Student.find(params[:id])
     erb(:edit)
   end
 # update
-  post '/students/:id' do
+  post '/students/:id/?' do
     student = Student.new(params)
     student.update
     redirect to 'students/' + params[:id]
   end
 
 # destroy
-post '/students/:id/delete' do
+post '/students/:id/delete/?' do
   @student = Student.find(params['id'])
   @student.delete
   redirect to 'students'
