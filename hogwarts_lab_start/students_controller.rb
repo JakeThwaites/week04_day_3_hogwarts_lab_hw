78339aec83fa65256e7cf07aa6203fde891381fe
Ernest Fraiman <upapaus@gmail.com>
2018-11-07 15:54:16 +0000

    started CRUD methods

--- hogwarts_lab_start/students_controller.rb
+++ hogwarts_lab_start/students_controller.rb
@@ -5,15 +5,38 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
-
-# show
+  get '/students' do
+    @students = Student.all()
+    erb(:index)
+  end
 
 # new
+  get '/students/new' do
+    erb(:new)
+  end
+# show
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb(:show)
+end
 
 # create
-
+post '/students' do
+  @student = Student.new(params)
+  @student.save()
+  redirect to '/students'
+end
 # edit
-
+  get '/students/:id/edit' do
+    @houses = [1, 2, 3, 4]
+    @student = Student.find(params[:id])
+    erb(:edit)
+  end
 # update
+  get '/students/:id/' do
+    student = Student.new(params)
+    student.update
+    redirect to 'students/' + params[:id]
+  end
 
 # destroy
